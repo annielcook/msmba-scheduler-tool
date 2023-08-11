a5c7e8fdb10bd914ca4a6f1a4d7e704610601b4d
Rebecca Sichel <17985323+rebecca-sich@users.noreply.github.com>
2023-08-11 00:37:06 -0400

    Update tests and docs

--- src/SEASCourses.test.js
+++ src/SEASCourses.test.js
@@ -1,181 +1,317 @@
-import {exportedForTesting} from './SEASCourses';
+import {
+    exportedForTesting
+} from './SEASCourses';
 
-const {getCompleteRawCoursesInfo, getRelevantSemestersInfo, parseCourse} = exportedForTesting;
+const {
+    getCompleteRawCoursesInfo,
+    getRelevantSemestersInfo,
+    parseCourse
+} = exportedForTesting;
 
-const dummyCourseList = [
-    {
-        "area": "ACS",
-        "courseNumber": "AC 207",
-        "title": "Systems Development for Computational Science",
-        "semesters": [
-            {
-                "academicYear": 2024,
-                "term": "Spring",
-                "instructors": [{"firstName": "Fabian", "lastName": "Wermelinger"}],
-                "offeredStatus": "Yes"
-            },
-            {
-                "academicYear": 2024,
-                "term": "Spring",
-                "instructors": [],
-                "offeredStatus": ""
-            },
-            {
-                "academicYear": 2024,
-                "term": "Fall",
-                "instructors": [],
-                "offeredStatus": ""
-            }],
-        "prefix": "AC",
-        "id": null
-    },
-    {
-        "area": "ACS",
-        "courseNumber": "AC 209a",
-        "title": "Data Science 1: Introduction to Data Science",
-        "semesters": [
-            {
-                "academicYear": 2023,
-                "term": "Fall",
-                "instructors": [{"lastName": "Protopapas", "firstName": "Pavlos"}],
-                "offeredStatus": "Yes"
-            },
-            {
-                "academicYear": 2022,
-                "term": "Fall",
-                "instructors": [],
-                "offeredStatus": ""
-            }],
-        "prefix": "AC",
-        "id": null
-    }
-];
-const dummyCourseTimes = [
-    {
-        "_id": "5eda9456a226a54703503873",
-        "area": "ACS",
-        "courseNumber": "AC 207",
+const dummyCourseList = [{
+    "id": "8a55f601-57ae-435b-8cae-4df4ddc611a7",
+    "catalogPrefix": "AC",
+    "catalogNumber": "AC 207",
+    "title": "Systems Development for Computational Science",
+    "sameAsId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+    "semesters": [{
+        "id": "e52e2cef-cf3e-4914-bf82-b846222c2b5b",
+        "calendarYear": "2023",
+        "academicYear": "2024",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "b90aa451-b510-472b-9bdd-a8e9d090eba7",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": [{
+                "id": "266dd997-1f5b-4af0-9bf1-92e0eb6c76b8",
+                "displayName": "Becker Troncoso, Ignacio",
+                "notes": "",
+                "instructorOrder": 0
+            }]
+        }
+    }, {
+        "id": "8d43d6cf-9280-435e-a566-1401557cf988",
+        "calendarYear": "2024",
+        "academicYear": "2024",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "c2d614a2-0f9c-4bb4-a558-95de6fff4a3f",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": []
+        }
+    }, {
+        "id": "a4908206-b425-4c71-9d34-901f973bb3c5",
+        "calendarYear": "2024",
+        "academicYear": "2025",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "1cd177cb-1516-4f7a-af25-c932d340eba5",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": []
+        }
+    }, {
+        "id": "40cec0c8-b6a9-427f-8c6e-46d92417044c",
+        "calendarYear": "2025",
+        "academicYear": "2025",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "74e97bde-8a83-4a77-a232-d95501908222",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": []
+        }
+    }, {
+        "id": "b23e5759-4e35-41f8-9a99-01064bc789a7",
+        "calendarYear": "2025",
+        "academicYear": "2026",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "b1662484-946d-4c3e-b584-f973b68928dd",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": []
+        }
+    }, {
+        "id": "be8e81de-d370-4d9e-9d42-68a9459e5496",
+        "calendarYear": "2026",
+        "academicYear": "2026",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "d8bf30e1-2a75-407b-9f51-7fc890bc1366",
+            "courseId": "df1b5916-9345-4c80-972c-d9bb7bd89d35",
+            "faculty": []
+        }
+    }]
+}, {
+    "id": "ce74b371-cf13-4108-a61b-993191079429",
+    "catalogPrefix": "AC",
+    "catalogNumber": "AC 209b",
+    "title": "Data Science 2: Advanced Topics in Data Science",
+    "sameAsId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+    "semesters": [{
+        "id": "e52e2cef-cf3e-4914-bf82-b846222c2b5b",
+        "calendarYear": "2023",
+        "academicYear": "2024",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "d6a1c1c4-0a42-4509-bbb3-dc5a7b50bbaf",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": []
+        }
+    }, {
+        "id": "8d43d6cf-9280-435e-a566-1401557cf988",
+        "calendarYear": "2024",
+        "academicYear": "2024",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "78568e90-1a00-49f9-b9f0-82a1a41eaaf5",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": [{
+                "id": "6d9e2a9d-e8c4-48bd-963b-bdffa532ba40",
+                "displayName": "Protopapas, Pavlos",
+                "notes": null,
+                "instructorOrder": 0
+            }, {
+                "id": "6de9b067-277f-4047-9236-90507c5748aa",
+                "displayName": "Young, Alex",
+                "notes": "",
+                "instructorOrder": 1
+            }]
+        }
+    }, {
+        "id": "a4908206-b425-4c71-9d34-901f973bb3c5",
+        "calendarYear": "2024",
+        "academicYear": "2025",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "955d895f-1b17-42c0-a0b4-4293d14f76f4",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": []
+        }
+    }, {
+        "id": "40cec0c8-b6a9-427f-8c6e-46d92417044c",
+        "calendarYear": "2025",
+        "academicYear": "2025",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "e94935b6-403c-4286-a867-79aef3bbf992",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": []
+        }
+    }, {
+        "id": "b23e5759-4e35-41f8-9a99-01064bc789a7",
+        "calendarYear": "2025",
+        "academicYear": "2026",
+        "term": "FALL",
+        "termOrder": 1,
+        "instance": {
+            "id": "ce8a19e5-8a27-4a6b-824f-caa593e9f6a9",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": []
+        }
+    }, {
+        "id": "be8e81de-d370-4d9e-9d42-68a9459e5496",
+        "calendarYear": "2026",
+        "academicYear": "2026",
+        "term": "SPRING",
+        "termOrder": 2,
+        "instance": {
+            "id": "185835c6-3b24-490f-b787-9a68315a4092",
+            "courseId": "de3531df-1f07-4a1c-8753-cbd5641bf0f2",
+            "faculty": []
+        }
+    }]
+}];
+
+const dummyCourseTimes = [{
+    "id": "ACTUE14151530FALL2023",
+    "coursePrefix": "AC",
+    "weekday": "TUE",
+    "startHour": 14,
+    "startMinute": 15,
+    "endHour": 15,
+    "endMinute": 30,
+    "duration": 75,
+    "courses": [{
+        "id": "c176d2be-9873-4bb8-bd5c-36a51e21d5bc",
+        "instanceId": "b90aa451-b510-472b-9bdd-a8e9d090eba7",
+        "courseNumber": "207",
         "isUndergraduate": false,
-        "semesters": [
-            {
-                "academicYear": 2020,
-                "term": "Fall",
-                "times": [{"days": "TR", "startTime": 720, "endTime": 795, "_id": "5f29c8e6f6dc37942e4896d1"}]
-            },
-            {
-                "academicYear": 2021,
-                "term": "Fall",
-                "times": [{"days": "TR", "startTime": 765, "endTime": 840, "_id": "610acc2b64b4c5f30df44f90"}]
-            },
-            {
-                "academicYear": 2024,
-                "term": "Spring",
-                "times": [{"days": "TR", "startTime": 855, "endTime": 930, "_id": "62d167c0d131374671a25d0a"}]
-            }
-        ],
-        "prefix": "AC",
-        "id": "5eda9456a226a54703503873"
-    },
-    {
-        "_id": "570cf330e3826cab6f6d94f4",
-        "area": "ACS",
-        "courseNumber": "AC 209a",
+        "campus": "Allston",
+        "room": "SEC 1.321"
+    }]
+}, {
+    "id": "ACTHU14151530FALL2023",
+    "coursePrefix": "AC",
+    "weekday": "THU",
+    "startHour": 14,
+    "startMinute": 15,
+    "endHour": 15,
+    "endMinute": 30,
+    "duration": 75,
+    "courses": [{
+        "id": "e3cc8d9d-1d1c-4f8c-ba0d-f90f6456b7f2",
+        "instanceId": "b90aa451-b510-472b-9bdd-a8e9d090eba7",
+        "courseNumber": "207",
         "isUndergraduate": false,
-        "semesters": [{
-            "academicYear": 2016,
-            "term": "Fall",
-            "times": [{"days": "MW", "startTime": 780, "endTime": 870, "_id": "5751c832cf1d903e2cc7ec22"}]
-        }, {
-            "academicYear": 2017,
-            "term": "Fall",
-            "times": [{"days": "MW", "startTime": 780, "endTime": 870, "_id": "58caa9abfa0b7c205c4f8b5f"}]
-        }, {
-            "academicYear": 2018,
-            "term": "Fall",
-            "times": [{"days": "MW", "startTime": 810, "endTime": 885, "_id": "5ab42b331b3c83287a79646b"}]
-        }, {
-            "academicYear": 2019,
-            "term": "Fall",
-            "times": [{"days": "MW", "startTime": 810, "endTime": 885, "_id": "5c8298dba54e871c101275a0"}]
-        }, {
-            "academicYear": 2020,
-            "term": "Fall",
-            "times": [{
-                "days": "MWF",
-                "startTime": 540,
-                "endTime": 615,
-                "_id": "5f40250fed7b833f52f2128e"
-            }, {"days": "MWF", "startTime": 900, "endTime": 975, "_id": "5f40250fed7b833f52f2128d"}]
-        }, {
-            "academicYear": 2021,
-            "term": "Fall",
-            "times": [{"days": "MWF", "startTime": 585, "endTime": 660, "_id": "610afe01457c4cc70a92ef8e"}]
-        }, {
-            "academicYear": 2023,
-            "term": "Fall",
-            "times": [{"days": "MWF", "startTime": 585, "endTime": 660, "_id": "62d16f15e4da944571e2b202"}]
-        }],
-        "prefix": "AC",
-        "id": "570cf330e3826cab6f6d94f4"
-    }
-];
+        "campus": "Allston",
+        "room": "SEC 1.321"
+    }]
+}, {
+    "id": "ACMON09451100SPRING2024",
+    "coursePrefix": "AC",
+    "weekday": "MON",
+    "startHour": 9,
+    "startMinute": 45,
+    "endHour": 11,
+    "endMinute": 0,
+    "duration": 75,
+    "courses": [{
+        "id": "dc775ad9-4843-47cb-a658-140a331a618e",
+        "instanceId": "78568e90-1a00-49f9-b9f0-82a1a41eaaf5",
+        "courseNumber": "209b",
+        "isUndergraduate": false,
+        "campus": "Allston",
+        "room": "SEC 1.321"
+    }]
+}, {
+    "id": "ACWED09451100SPRING2024",
+    "coursePrefix": "AC",
+    "weekday": "WED",
+    "startHour": 9,
+    "startMinute": 45,
+    "endHour": 11,
+    "endMinute": 0,
+    "duration": 75,
+    "courses": [{
+        "id": "4f41c4fa-44ef-4903-98c8-9c57b5c9e9c9",
+        "instanceId": "78568e90-1a00-49f9-b9f0-82a1a41eaaf5",
+        "courseNumber": "209b",
+        "isUndergraduate": false,
+        "campus": "Allston",
+        "room": "SEC 1.321"
+    }]
+}, {
+    "id": "ACFRI09451100SPRING2024",
+    "coursePrefix": "AC",
+    "weekday": "FRI",
+    "startHour": 9,
+    "startMinute": 45,
+    "endHour": 11,
+    "endMinute": 0,
+    "duration": 75,
+    "courses": [{
+        "id": "b7e4c36c-62e9-433a-9c8d-e5adc9c56c51",
+        "instanceId": "78568e90-1a00-49f9-b9f0-82a1a41eaaf5",
+        "courseNumber": "209b",
+        "isUndergraduate": false,
+        "campus": "Allston",
+        "room": "SEC 1.321"
+    }]
+}];
 
 test('Get full course info', async () => {
     const result = getCompleteRawCoursesInfo(dummyCourseList, dummyCourseTimes);
 
     const columns = Object.keys(result[0]);
     expect(columns).toEqual(
-        expect.arrayContaining(["area", "courseNumber", "title", "semesters",
-            "prefix", "id", "semesterTimes", "isUndergraduate"])
+        expect.arrayContaining(["id", "catalogPrefix", "catalogNumber", "title", "sameAsId",
+            "semesters", "semesterTimes", "isUndergraduate"
+        ])
     );
 });
 
 test('Get course semesters info', () => {
     const toTest = getCompleteRawCoursesInfo(dummyCourseList, dummyCourseTimes);
-    const resultOfferedSpring = getRelevantSemestersInfo(toTest[0]);
-    const resultOfferedFall = getRelevantSemestersInfo(toTest[1]);
+
+    const resultOfferedFall = getRelevantSemestersInfo(toTest[0]);
+    const resultOfferedSpring = getRelevantSemestersInfo(toTest[1]);
 
     expect(resultOfferedSpring.length).toEqual(1);
     expect(resultOfferedFall.length).toEqual(1);
-
-    expect(resultOfferedSpring[0].instructors).toEqual(
-        expect.arrayContaining(dummyCourseList[0].semesters[0].instructors)
-    );
-    expect(resultOfferedSpring[0].times).toEqual(
-        expect.arrayContaining(dummyCourseTimes[0].semesters[2].times)
-    );
-
     expect(resultOfferedFall[0].instructors).toEqual(
-        expect.arrayContaining(dummyCourseList[1].semesters[0].instructors)
+        expect.arrayContaining(dummyCourseList[0].semesters[0].instance.faculty)
     );
-    expect(resultOfferedFall[0].times).toEqual(
-        expect.arrayContaining(dummyCourseTimes[1].semesters[6].times)
+    expect(resultOfferedSpring[0].instructors).toEqual(
+        expect.arrayContaining(dummyCourseList[1].semesters[1].instance.faculty)
     );
+    // Didnt test times data and probably could add but a bit multifaceted
+
 });
 
 test('Parse course', () => {
     const toTest = getCompleteRawCoursesInfo(dummyCourseList, dummyCourseTimes);
-    const resultSpring = parseCourse(toTest[0]);
+    const resultSpring = parseCourse(toTest[1]);
     expect(resultSpring.length).toEqual(1);
     const resultSpringInfo = resultSpring[0];
     expect(resultSpringInfo.semester).toEqual("S");
     expect(resultSpringInfo.school).toEqual("SEAS");
-    expect(resultSpringInfo.prof).toEqual("Fabian Wermelinger");
-    expect(resultSpringInfo.name).toEqual(`${dummyCourseList[0].courseNumber} ${dummyCourseList[0].title}`);
-    expect(resultSpringInfo.id).toEqual(`SEAS-Spring-${dummyCourseTimes[0].courseNumber.replace(' ', '-')}`);
+    expect(resultSpringInfo.prof).toEqual("Protopapas, Pavlos, Young, Alex");
+    expect(resultSpringInfo.name).toEqual(`${dummyCourseList[1].catalogNumber} ${dummyCourseList[1].title}`);
+    expect(resultSpringInfo.id).toEqual(`SEAS-SPRING-${dummyCourseList[1].catalogNumber.replace(' ', '-')}`);
     expect(resultSpringInfo.times).toEqual(
-        expect.arrayContaining([
-            {
-                day: 1,
-                startHour: "14:15",
-                endHour: "15:30"
-            },
-            {
-                day: 3,
-                startHour: "14:15",
-                endHour: "15:30"
-            }
-        ])
+        expect.arrayContaining([{
+            day: 0,
+            startHour: "09:45",
+            endHour: "11:00"
+        }, {
+            day: 2,
+            startHour: "09:45",
+            endHour: "11:00"
+        },
+        {
+            day: 4,
+            startHour: "09:45",
+            endHour: "11:00"
+        }])
     )
-    expect(resultSpringInfo.timeText).toEqual("Tue, Thu 14:15-15:30");
+    expect(resultSpringInfo.timeText).toEqual("Mon, Wed, Fri 09:45-11:00");
 });
